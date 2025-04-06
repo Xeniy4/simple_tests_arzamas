a53b536eab45675ca06b6eed72a9fa30c0387adb
Goryaeva Kseniya <goryaevakss@gmail.com>
2025-04-06 13:49:35 +0300

    add tests in test_content_kids.py.py

--- pages/page_main.py
+++ pages/page_main.py
@@ -109,11 +109,17 @@ class GoosegooseKids:
         # value = all, courses, podcasts, audio_materials,
 
     def select_course_kids(self):
-        browser.element('[href="/kids/427"]').click()
+        browser.element('[href="/kids/480"]').click()
+        # Съедобное/несъедобное
 
     def select_podcast_kids(self):
         browser.element('[href="/kids/416"]').click()
 
     def select_audio_material_kids(self):
         browser.element('[href="/kids/474"]').click()
+
+    def check_tutle_content_page_kids(self, value):
+        browser.element('.card__title').should(have.text(value))
+
+
 
--- tests/test_content_kids.py
+++ tests/test_content_kids.py
@@ -1,2 +1,60 @@
 import allure
 from pages.page_main import OpenPage, GoosegooseKids
+
+
+open_page = OpenPage()
+kigs = GoosegooseKids()
+
+def test_courses_kids():
+
+    with allure.step('Открыть Главную страницу'):
+        open_page.open()
+
+    with allure.step('Нажать на кнопку в хедер меню "Гусьгусь"'):
+        open_page.button_goosegoose()
+
+    with allure.step('Открыть раздел "Курсы"'):
+        kigs.navigation_list_kids('courses')
+
+    with allure.step('Открыть курс "Съедобное/несъедобное"'):
+        kigs.select_course_kids()
+
+    with allure.step('Проверка отображения заголовка курса'):
+        kigs.check_tutle_content_page_kids('Съедобное/несъедобное')
+
+
+def test_podcast_kids():
+    with allure.step('Открыть Главную страницу'):
+        open_page.open()
+
+    with allure.step('Нажать на кнопку в хедер меню "Гусьгусь"'):
+        open_page.button_goosegoose()
+
+    with allure.step('Открыть раздел "Подкасты"'):
+        kigs.navigation_list_kids('podcasts')
+
+    with allure.step('Открыть подкаст "Это вам не сказки"'):
+        kigs.select_podcast_kids()
+
+    with allure.step('Проверка отображения заголовка подкаста'):
+        kigs.check_tutle_content_page_kids('Это вам не сказки')
+
+
+def test_audio_kids():
+    with allure.step('Открыть Главную страницу'):
+        open_page.open()
+
+    with allure.step('Нажать на кнопку в хедер меню "Гусьгусь"'):
+        open_page.button_goosegoose()
+
+    with allure.step('Открыть раздел "Аудиокниги"'):
+        kigs.navigation_list_kids('audio_materials')
+
+    with allure.step('Открыть аудиокнигу "Мифы звездного неба"'):
+        kigs.select_audio_material_kids()
+
+    with allure.step('Проверка отображения заголовка аудиокниги'):
+        kigs.check_tutle_content_page_kids('Мифы звездного неба')
+
+
+
