981e660993bd3a5fac28d1f3046f081ed5e8fe58
Goryaeva Kseniya <goryaevakss@gmail.com>
2025-06-02 18:30:34 +0300

    delete fixture browser_manager

--- tests/conftest.py
+++ tests/conftest.py
@@ -37,21 +37,29 @@ def selenoid():
         options=options)
 
     browser.config.driver = driver
-    yield
-    attach.add_html(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
-    attach.add_screenshot(browser)
 
-
-@pytest.fixture(scope='session', autouse=True)
-def browser_manager():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'normal'
     browser.config.driver_options = driver_options
     browser.config.base_url = "https://arzamas.academy"
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    #browser.config.timeout = 120
     yield
+    attach.add_html(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
+    attach.add_screenshot(browser)
     browser.quit()
+
+
+# @pytest.fixture(scope='session', autouse=True)
+# def browser_manager():
+#     driver_options = webdriver.ChromeOptions()
+#     driver_options.page_load_strategy = 'normal'
+#     browser.config.driver_options = driver_options
+#     browser.config.base_url = "https://arzamas.academy"
+#     browser.config.window_width = 1920
+#     browser.config.window_height = 1080
+#     #browser.config.timeout = 120
+#     yield
+#     browser.quit()
